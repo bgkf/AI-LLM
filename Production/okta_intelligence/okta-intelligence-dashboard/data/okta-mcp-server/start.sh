d8fab173e1d2bccc1d9bcc323f4fafbf6530b8db
bgkf <108151241+bgkf@users.noreply.github.com>
2026-05-08 07:25:51 -0700

    Delete Production/okta_intelligence directory
    replaced with Okta_MCP_Dashboard

--- Production/okta_intelligence/okta-intelligence-dashboard/data/okta-mcp-server/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec "/Users/USERNAME/Library/Application Support/OktaDashboard/venv/bin/python" "/Users/USERNAME/Library/Application Support/OktaDashboard/data/okta-mcp-server/server.py"
